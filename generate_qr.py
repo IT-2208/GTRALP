a0b862f2fe0e4fe6209239565a73e014a1a3bef5
IT-2208 <it@gtratech.com>
2025-08-20 14:22:31 +0530

    Update generate_qr.py

--- generate_qr.py
+++ generate_qr.py
@@ -6,8 +6,8 @@ os.makedirs('GTRALP', exist_ok=True)
 base_url = 'https://it-2208.github.io/GTRALP/'
 
 for i in range(1, 16):  # 1 to 15 inclusive
-    file_html = f'GTRALP/GTRALP{i:03}.html'      # zero-padded: GTRALP001.html
-    file_png = f'GTRALP/GTRALP{i:03}_qr.png'     # QR code file
+    file_html = f'GTRALP/GTRALP{i:03}.html'      # GTRALP001.html etc.
+    file_png = f'GTRALP/GTRALP{i:03}_qr.png'     # GTRALP001_qr.png etc.
     full_url = base_url + file_html
     img = qrcode.make(full_url)
     img.save(file_png)
