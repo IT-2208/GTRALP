e557131791d2a0830db6ff2cc3ca8c828c26b695
IT-2208 <it@gtratech.com>
2025-08-20 13:57:11 +0530

    Update generate_qr.py

--- generate_qr.py
+++ generate_qr.py
@@ -1,4 +1,8 @@
 import qrcode
+import os
+
+# Create folder if it doesn't exist
+os.makedirs('GTRALP', exist_ok=True)
 
 url = 'https://it-2208.github.io/GTRALP/GTRALP002.html'
 img = qrcode.make(url)
